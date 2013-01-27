4e6dfdb5434bf73571558164069d17a353048bdd
my ubuntu <tingleshao@gmail.com>
2013-01-26 23:30:57 -0500

    ?

--- ch04/ch04.exercises.hs
+++ ch04/ch04.exercises.hs
@@ -1,1 +1,13 @@
 -- file ch04/ch04.exercises.hs
+safeHead :: [a] -> Maybe a
+safeTail :: [a] -> Maybe [a]
+safeLast :: [a] -> Maybe a
+safeInit :: [a] -> Maybe [a]
+
+safeListFunc func [] = Nothing
+safeListFunc func xs = Just (func xs)
+
+safeHead = safeListFunc head
+safeTail = safeListFunc tail
+safeLast = safeListFunc last
+safeInit = safeListFunc init
